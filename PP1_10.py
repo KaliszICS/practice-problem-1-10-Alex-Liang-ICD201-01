69236a0dd52ee02abb9fb845be70d9aeb9a77dfc
Alex-Liang-ICD201-01 <alex.liang3@student.tdsb.on.ca>
2024-10-02 13:48:17 +0000

    Commit

--- PP1_10.py
+++ PP1_10.py
@@ -1,31 +1,49 @@
 
+''' 
+    Lesson: Meth Library
+    Author: Alex Liang
+    Date Created: October 2nd, 2024
+    Date Last Modified: October 2nd, 2024
+'''
+
+import math 
 
 def q1(): 
   #Write Assignment code here
 
+  num1 = input("Input a number: ")
+  print(math.sqrt(float(num1)))
 
 def q2(): 
   #Write Assignment code here
 
+  num2 = int(input("Input a number: "))
+  print(math.isqrt(num2))
 
 def q3(): 
   #Write Assignment code here
 
+  num3 = input("Input a number: ")
+  print(math.floor(float(num3)))
 
 def q4(): 
   #Write Assignment code here
 
+  num4 = input("Input a number: ")
+  print(math.ceil(float(num4)))
 
 def q5(): 
   #Write Assignment code here
 
-
+  num5 = float(input("Input a number: "))
+  num6 = float(input("Input another number: "))
+  print(math.floor(num5 * num6 / 2))
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
